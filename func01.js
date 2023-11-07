2882ee8bbeb74839b3c20f6bd8eccd3f3264365d
Natsunsn <Natsunan270@gmail.com>
2023-11-07 15:26:01 +0700

    first commit

---
+++ func01.js
@@ -0,0 +1,9 @@
+//Define function
+function myFunction() {
+    console.log("My First Function");
+  }
+  //Call function
+  myFunction();
+  
+  document.getElementById("func1").innerHTML = "My First Function";
+  
---
+++ func08.js
@@ -0,0 +1,13 @@
+//1.Define function
+const greet = () => {
+    console.log("Hi");
+  };
+  
+  //1.Define function
+  const inUser = (callback, msg, name) => {
+    callback();
+    console.log(msg, name);
+  };
+  //2.Call function
+  inUser(greet, "Hello", "Mark");
+  
